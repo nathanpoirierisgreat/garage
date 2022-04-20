b7b07ee415913edc8b0a03e9669dfa0ff272f54a
nathanpoirierisgreat <100322123+nathanpoirierisgreat@users.noreply.github.com>
2022-04-20 17:08:12 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "up") {
+    if (receivedString == "back") {
         servos.P1.setAngle(90)
     }
-    if (receivedString == "down") {
+    if (receivedString == "foreword") {
         servos.P1.setAngle(0)
     }
 })
