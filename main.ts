022e21523d0a82865c96ebaa065e82a8eaf48ee5
nathanpoirierisgreat <100322123+nathanpoirierisgreat@users.noreply.github.com>
2022-04-27 15:38:20 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,12 @@ servos.P1.setAngle(91)
     basic.pause(5000)
     basic.pause(5000)
     basic.showString("closing",75)
-servos.P1.setAngle(180)
+radio.sendString("close garage")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "close garage") {
+        servos.P1.setAngle(180)
+    }
 })
 basic.forever(function () {
     radio.setGroup(8)
